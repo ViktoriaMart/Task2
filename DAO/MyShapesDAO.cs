2ad77865c51b86d8106b68c7a499f3ae6c8c4ad5
ViktoriaMart <47144982+ViktoriaMart@users.noreply.github.com>
2019-01-31 20:41:41 +0200

    Added DAO

---
+++ DAO/MyShapesDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Models;
+using System.Xml.Serialization;
+using System.Collections.ObjectModel;
+
+namespace DAO
+{
+    public class MyShapesDAO
+    {
+        public void saveShapes(string fileName, ObservableCollection<MyShape> MyShapesCollection)
+        {
+            XmlSerializer formatter = new XmlSerializer(MyShapesCollection.GetType(), new Type[] { typeof(MyPolygon)});
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Create))
+            {
+                formatter.Serialize(fs, MyShapesCollection);
+            }
+        }
+
+        public ObservableCollection<MyShape> getShapes(string fileName)
+        {
+            ObservableCollection<MyShape> MyShapesCollection;
+
+            XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<MyShape>), new Type[] { typeof(MyPolygon)});
+
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                MyShapesCollection = (ObservableCollection<MyShape>)formatter.Deserialize(fs);
+            }
+
+            return MyShapesCollection;
+        }
+    }
+}
